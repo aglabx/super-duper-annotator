4119336d2b613bdd303c7f2f67beffd03131649a
Sbr7 <barua@scamt-itmo.ru>
2021-07-01 16:05:21 +0300

    script file for barrnap

---
+++ scripts/barrnap.py
@@ -0,0 +1,3 @@
+def barrnap_command(barrnap_binary, barrnap_input, barrnap_output, barrnap_options):
+    barrnap_command = f"{barrnap_options} {barrnap_input} {barrnap_output}"
+    return barrnap_command
